8ff5dea37212deaa6a887053ece4036f4517aded
Zihadul Islam <zynscriptofficial@gmail.com>
2023-06-04 12:25:34 +0600

    header & footer added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,12 +1,15 @@
 import {createBrowserRouter} from "react-router-dom";
+import Home from "../pages/Home/Home/Home";
+import Main from "../Layout/Main";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Main></Main>,
+    element: <Main />,
     children: [
       {
         path: "/",
+        element: <Home />,
       },
     ],
   },
